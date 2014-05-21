351516590581c746c9b99e3376399f1746a11fb1
Fernando-Oliva <fernando.oliva.hueto@gmail.com>
2014-05-21 21:27:16 +0200

    First Commit

---
+++ dotPlayer/Core/Player.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotPlayer.Core
+{
+    public class Player
+    {
+        public Uri LoadFile()
+        {
+
+                OpenFileDialog ofdSearchFile = new OpenFileDialog();
+
+                ofdSearchFile.ShowDialog();
+
+                ofdSearchFile.Filter = "All Files (*.*)|*.*";
+                ofdSearchFile.FilterIndex = 1;
+                Uri uri = new Uri(ofdSearchFile.FileName);
+                
+
+                return uri;
+
+        }
+    }
+}
---
+++ dotPlayer/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using dotPlayer.Core;
+
+namespace dotPlayer
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Player videoPlayer = new Player();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            mScreen.Source = videoPlayer.LoadFile();
+        }
+    }
+}
